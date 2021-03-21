3f97dc5e2b5d55e958b09d2168091442f814bd37
David Bytyqi <david.bytyqi1@outlook.com>
2021-03-21 11:10:03 +0100

    Add project files.

---
+++ EmExpressProject/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EmExpressProject.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EmExpressProject.Areas.Identity.IdentityHostingStartup))]
+namespace EmExpressProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
